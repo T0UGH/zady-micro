851d015800df28b2311a86be86b1c42073aaf9b6
t0ugh <1243384249@qq.com>
2020-04-30 08:42:03 +0800

    Feat: 公共模块

---
+++ zady-micro-common/src/main/java/com/edu/neu/zadymicrocommon/ZadyMicroCommonApplication.java
@@ -0,0 +1,13 @@
+package com.edu.neu.zadymicrocommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadyMicroCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadyMicroCommonApplication.class, args);
+    }
+
+}
